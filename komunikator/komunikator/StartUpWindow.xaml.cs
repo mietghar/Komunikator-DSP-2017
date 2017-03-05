220352210f865566732fa7650336a879d898f2dc
mietghar <michal@mietech.pl>
2017-03-05 19:35:06 +0100

    Pierwsza wersja aplikacji z wielowątkowym rozwiązaniem klient \ serwer.

---
+++ komunikator/komunikator/StartUpWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace komunikator
+{
+    /// <summary>
+    /// Interaction logic for StartUpWindow.xaml
+    /// </summary>
+    public partial class StartUpWindow : Window
+    {
+        public StartUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btClientRun_Click(object sender, RoutedEventArgs e)
+        {
+            Thread klient = new Thread(delegate ()
+            {
+                MainWindow Klient = new MainWindow();
+                Klient.Show();
+                System.Windows.Threading.Dispatcher.Run();
+            });
+            klient.SetApartmentState(ApartmentState.STA);
+            klient.Start();
+        }
+
+        private void btnServerRun_Click(object sender, RoutedEventArgs e)
+        {
+            Thread serwer = new Thread(delegate ()
+            {
+                TCPServer Serwer = new TCPServer();
+                Serwer.Show();
+                System.Windows.Threading.Dispatcher.Run();
+            });
+            serwer.SetApartmentState(ApartmentState.STA);
+            serwer.Start();
+        }
+    }
+}
---
+++ komunikator/komunikator/TCPServer.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace komunikator
+{
+    /// <summary>
+    /// Interaction logic for TCPServer.xaml
+    /// </summary>
+    public partial class TCPServer : Window
+    {
+        //pola prywatne
+        private TcpListener serwer;
+        private TcpClient klient;
+
+        public TCPServer()
+        {
+            InitializeComponent();
+            IPAddress[] LocalIPS = GetLocalIPs();
+            foreach (IPAddress ip in LocalIPS)
+            {
+                listBox.Items.Add(ip.ToString());
+            }
+            tbAdresSerwera.Text = LocalIPS[0].ToString();
+        }
+
+        private IPAddress[] GetLocalIPs()
+        {
+            List<IPAddress> list = new List<IPAddress>();
+            IPAddress[] AdresIPSerwera = null;
+            IPHostEntry host = null;
+            try
+            {
+                host = Dns.GetHostEntry(Dns.GetHostName());
+            }
+            catch
+            {
+                return null;
+            }
+            foreach (IPAddress IP in host.AddressList)
+            {
+                if (IP.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    list.Add(IPAddress.Parse(IP.ToString()));
+                }
+            }
+            AdresIPSerwera = list.ToArray();
+            return AdresIPSerwera;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            int port = 20001;
+            int i = 0;
+            try
+            {
+                //AdresIPSerwera = IPAddress.Parse(localIP.ToString());
+                //AdresIPSerwera = ((IPEndPoint)s.LocalEndPoint).Address.ToString())
+                //serwer = new TcpListener(AdresIPSerwera, port);
+                serwer = new TcpListener(IPAddress.Parse("192.168.0.104"), port);
+                serwer.Start();
+                klient = serwer.AcceptTcpClient();
+                IPEndPoint IP = (IPEndPoint)klient.Client.RemoteEndPoint;
+                listBox.Items.Add(IP.ToString() + " Nawiązano połączenie");
+            }
+            catch
+            {
+                listBox.Items.Add("Coś poszło nie tak.");
+            }
+        }
+    }
+}
