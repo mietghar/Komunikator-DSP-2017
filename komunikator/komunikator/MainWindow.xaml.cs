2ed130c7e3b830bdece0d64135a738d14c98e288
mietghar <michal@mietech.pl>
2017-03-05 18:24:11 +0100

    Pierwszy interfejs działającego klienta komunikatora. Komunikacja synchroniczna TCP.

--- komunikator/komunikator/MainWindow.xaml.cs
+++ komunikator/komunikator/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,47 @@ namespace komunikator
         public MainWindow()
         {
             InitializeComponent();
+            SetMyIPAddress();
+        }
+
+        IPAddress adresIPHost;
+        IPAddress adresIPClient;
+
+        private void SetMyIPAddress()
+        {
+            //pobranie mojej nazwy komputera - hosta
+            try
+            {
+                IPHostEntry MyComputer = Dns.GetHostEntry(Dns.GetHostName());
+                //odczytanie mojego ip na podstawie nazwy hosta
+                foreach (var ip in MyComputer.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                         MyIPAddress.Text=ip.ToString();
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            string host = myPartnerAddressTextBox.Text;
+            int port = 20001;
+            try
+            {
+                TcpClient klient = new TcpClient(host, port);
+                EventsListBox.Items.Add("Nawiązano połącznie z serwerem.");
+                klient.Close();
+            }
+            catch(Exception ex)
+            {
+                EventsListBox.Items.Add("Błąd" + ex.ToString());
+                //MessageBox.Show("Błąd." +ex.ToString());
+            }
         }
     }
 }
---
+++ komunikator/komunikator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace komunikator.ViewModels
+{
+    class MainWindowViewModel : BindableBase
+    {
+        private readonly string _defaultMyIPAddress = "127.0.0.1";
+        #region Properties
+        private string _myIPAddress;
+
+        public string myIPAddress
+        {
+            get
+            {
+                return SetMyIPAddress();
+                //return !string.IsNullOrEmpty(_myIPAddress) ? _myIPAddress : _defaultMyIPAddress;
+            }
+            set
+            {
+                value = _defaultMyIPAddress;
+            }
+        }
+
+        private string SetMyIPAddress()
+        {
+            //pobranie mojej nazwy komputera - hosta
+            try
+            {
+                IPHostEntry MyComputer = Dns.GetHostEntry(Dns.GetHostName());
+                //odczytanie mojego ip na podstawie nazwy hosta
+                foreach (var ip in MyComputer.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        return ip.ToString();
+                    }
+                }
+                return "";
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+        public MainWindowViewModel()
+        {
+            myIPAddress=myIPAddress;
+        }
+        #endregion
+    }
+}
